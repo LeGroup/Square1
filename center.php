8eafc413f81a9deff62a75a46e244844e2201617
Antti Keränen <detegr@gmail.com>
2014-01-10 14:34:30 +0200

    Area example

--- center.php
+++ center.php
@@ -1,9 +1,77 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>	
+		<meta charset="UTF-8"></meta>
+
 		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Noto+Sans' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" type="text/css" href="style.css">
+
+		<script src="script/jquery.js"></script>
+		<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+		<script src="script/jQueryRotateCompressed.js"></script>
+		<script src="script/table.js"></script>
+
+		<style>
+			html,body {
+				width: 100%;
+				height: 100%;
+				-webkit-user-select: none;
+				-webkit-touch-callout: none;
+			}
+			#dummy { width: 100%; height: 40%; }
+			#top {
+				position: absolute;
+				top: 0;
+				left: 20%;
+				width: 60%;
+				height: 40%;
+				background-color: #EFEFEF;
+				margin-left: auto;
+				margin-right: auto;
+			}
+			#left {
+				float: left;
+				width: 20%;
+				height: 100%;
+				background-color: #EFDFEF;
+			}
+			#right {
+				float: right;
+				width: 20%;
+				height: 100%;
+				background-color: #EFDFEF;
+			}
+			#bottom {
+				position: absolute;
+				bottom: 0;
+				left: 20%;
+				width: 60%;
+				height: 40%;
+				background-color: #EFEFEF;
+				margin-top: auto;
+				margin-left: auto;
+				margin-right: auto;
+			}
+			.node {
+				margin: 10px;
+				padding: 10px;
+				display: inline-block;
+				white-space: pre-wrap;
+				box-shadow: 2px 2px 2px rgba(0,0,0,0.2);
+				background-color: white;
+				border: 1px solid #093;
+				max-width: 200px;
+				content: "↺";
+			}
+			.rotate {
+				display: none;
+				position: absolute;
+				/*top: -20px;*/
+				top: 0;
+				right: 4px;
+			}
+		</style>
 	</head>
 	<body>
 		<?php
@@ -17,12 +85,10 @@
 		?>
 		<div id="canvas"><div id="no-found">Empty room<br>Why don't try to <a href="?<?php echo $url; ?>">write a note</a>?</div></div>
 		<script type="text/javascript">var Room="<?php echo $_GET['room']; ?>";</script>
-		<script src="script/jquery-1.9.1.js" type="text/javascript"></script>
-		<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js" type="text/javascript"></script>
-		<script src="script/jquery.touch-punch.min.js" type="text/javascript"></script>
-		<script src="script/raphael-min.js" type="text/javascript"></script>
-		<script src="script/jquery.mousewheel.js" type="text/javascript"></script>
-		<script src="script/svg-navigation.js" type="text/javascript"></script>
-		<script src="script/center.js" type="text/javascript"></script>
+<div data-name="left" class="area" id="left"></div>
+<div data-name="right" class="area" id="right"></div>
+<div id="dummy"></div>
+<div data-name="top" class="area" id="top"></div>
+<div data-name="bottom" class="area" id="bottom"></div>
 	</body>
 </html>
---
+++ script/table.js
@@ -0,0 +1,163 @@
+(function() {
+
+"use strict";
+
+var lorem=[
+"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+"Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+"Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.",
+"Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.",
+"Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem.",
+"Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius.",
+"Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.",
+"Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima.",
+"Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum."];
+
+var maxID=0;
+
+function getNodes(max) {
+	$.post('fetch.php', { max: max, room: Room }, function(response) {
+		//console.log('PHP: ' + response);
+		var obj = JSON.parse(response);
+		var nodes = obj.nodes;
+		maxID = obj.max;
+		for(var node in nodes) {
+			addNode(nodes[node]);
+		}
+	});
+}
+
+function angle($elem, x, y)
+{
+	var eoff=$elem.offset();
+	var dx=x - (eoff.left + ($elem.width()/2));
+	var dy=y - (eoff.top + ($elem.height()/2));
+  return (Math.atan2(dy, dx) * 180 / Math.PI);
+}
+
+function rotate($elem, deg)
+{
+	$elem.css({
+		'-moz-transform':'rotate('+deg+'deg)',
+		'-webkit-transform':'rotate('+deg+'deg)',
+		'-o-transform':'rotate('+deg+'deg)',
+		'-ms-transform':'rotate('+deg+'deg)',
+		'transform': 'rotate('+deg+'deg)'
+	});
+}
+
+function getBbox(elem)
+{
+	var off=$(elem).offset();
+	return {
+		left: off.left,
+		right: off.left + $(elem).outerWidth(true),
+		top: off.top,
+		bottom: off.top + $(elem).outerHeight(true)
+	};
+}
+function insideBbox(bbox, x, y)
+{
+	return (x > bbox.left && x < bbox.right &&
+		y > bbox.top && y < bbox.bottom);
+}
+
+function addNode(node)
+{
+	$('#no-found').remove();
+	var $header=$("<header class='rotate'>↻</header>");
+	var header_rotating=false;
+	var starta=0;
+	var $node_to_rotate=null;
+	var lastr=0;
+	/*
+	$header.on("mousedown", function(e)
+	{
+		header_rotating=true;
+		$node_to_rotate=$(this).parent();
+		starta=$node_to_rotate.data("angle") ? $node_to_rotate.data("angle") : angle($node_to_rotate, e.pageX, e.pageY);
+		console.log(starta);
+		return false;
+	});
+	$(document).on("mousemove", function(e)
+	{
+		if(header_rotating)
+		{
+			lastr=starta-angle($node_to_rotate, e.pageX, e.pageY);
+			lastr;
+			rotate($node_to_rotate, starta-lastr);
+		}
+	}).on("mouseup", function(e)
+	{
+		header_rotating=false;
+		if($node_to_rotate)
+		{
+			$node_to_rotate.data("angle", lastr);
+		}
+	});
+	*/
+	var a=0;
+	/*
+	$header.on("click", function(e)
+	{
+		a=a+90 % 360;
+		$(this).parent().rotate({animateTo: a, duration: 250});
+	});
+	*/
+	var $e=$("<span class='node'>" + node.text + "</span>");
+	$e.append($header);
+	$e.click(function()
+	{
+		// Get the maximum z-index of nodes
+		var maxw=Math.max.apply(
+			this,
+			$(".node").map(function() { return parseInt($(this).css("zIndex"), 10); })
+		);
+		// Assign z-index of maximum + 1 to make this the topmost node.
+		$(this).css("zIndex", maxw+1);
+	});
+	$e.draggable({cursor: "move", stack: ".node", stop: function(e)
+	{
+		var $draggable=$(this);
+		$(".area").each(function()
+		{
+			if(insideBbox(getBbox(this), e.pageX, e.pageY))
+			{
+				var name=$(this).data("name");
+				switch(name)
+				{
+					case "bottom":
+						$draggable.rotate({animateTo: 0, duration: 250});
+						break;
+					case "left":
+						$draggable.rotate({animateTo: 90, duration: 250});
+						break;
+					case "top":
+						$draggable.rotate({animateTo: 180, duration: 250});
+						break;
+					case "right":
+						$draggable.rotate({animateTo: 270, duration: 250});
+						break;
+				}
+				return;
+			}
+		});
+	}});
+	/*
+	$e.mouseover(function()
+	{
+		$header.stop().fadeIn();
+	}).mouseleave(function()
+	{
+		$header.stop().fadeOut();
+	});
+	*/
+	$(document.body).append($e);
+}
+
+$(function() {
+	$(".node").remove();
+	getNodes(maxID);
+	setInterval(function() { GetNodes(maxID); }, 5000 );
+});
+})();
